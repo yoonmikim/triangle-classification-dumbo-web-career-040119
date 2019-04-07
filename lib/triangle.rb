6f76d97b445551278183d587e4eeaa6daecdc3c4
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-04-07 08:53:46 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,10 +2,14 @@ class Triangle
   # write code here
   attr_accessor :equilateral, :isosceles, :scalene
   
-  def kind(s1, s2, s3)
+  def initialize(s1, s2, s3)
     @s1 = s1
     @s2 = s2
     @s3 = s3
+  end
+  
+  def kind 
+    
   end
 
 
